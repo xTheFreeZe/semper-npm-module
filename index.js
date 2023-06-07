7457ff8a1f77bc942c40f666e64641af0a0c23c2
Marwin <marwin.eder@gmx.de>
2023-06-08 01:35:01 +0200

    Jetzt aber

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const testFunction = () => {
+const dotenv = require('dotenv');
+dotenv.config();
 
-    return "Hello World";
+/**
+ * @module getTeamData
+ * @file getTeamData.js
+ * @description Get all data for a team.
+ * @param {Number} teamId - The team ID.
+ * @param {String} guildId - The guild ID.
+ */
+const getTeamData = async (teamId, guildId) => {
 
-}
+  const url = `${process.env.SEMPER_API_URL}team/get/data/?teamId=${teamId}&guildId=${guildId}&accessToken=${process.env.SEMPER_API_ACCESS_TOKEN}`;
 
-module.exports = testFunction;
+  try {
+
+    const response = await fetch(url, { method: 'GET' });
+    const data = await response.json();
+
+    return data;
+
+  } catch (error) {
+
+    console.log(error);
+    return error;
+
+  }
+
+};
+
+module.exports = getTeamData;
