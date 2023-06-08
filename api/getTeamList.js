48f0b44a5635efa1eed23daf5241b614bb6890e0
Marwin <marwin.eder@gmx.de>
2023-06-08 11:35:37 +0200

    getTeamList()

---
+++ api/getTeamList.js
@@ -0,0 +1,38 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+/**
+ * @module getTeamList
+ * @file getTeamList.js
+ * @description Returns a list of all teams in a guild including their players.
+ * @param {Number} teamId - The team ID.
+ * @param {Number} start - The start index.
+ * @param {Number} end - The end index.
+ * @description getTeamList ('980516998670868530', 0, 10) - Returns the first 10 teams in the guild.
+ */
+const getTeamList = async (guildId, start, end) => {
+
+  if (guildId === undefined || start === undefined || end === undefined) {
+
+    return console.error('[ERROR] Missing parameters for getTeamList() function.');
+
+  }
+
+  const url = `${process.env.SEMPER_API_URL}teams/list?guildId=${guildId}&start=${start}&end=${end}&accessToken=${process.env.SEMPER_API_ACCESS_TOKEN}`;
+
+  try {
+
+    const response = await fetch(url, { method: 'GET' });
+    const data = await response.json();
+
+    return data;
+
+  } catch (error) {
+
+    console.error('[ERROR] Failed to fetch team list data from API:', error);
+    return;
+
+  }
+};
+
+module.exports = getTeamList;
--- index.js
+++ index.js
@@ -1,31 +1,4 @@
-const dotenv = require('dotenv');
-dotenv.config();
+const getTeamData = require('./api/getTeamData.js');
+const getTeamList = require('./api/getTeamList.js');
 
-/**
- * @module getTeamData
- * @file getTeamData.js
- * @description Get all data for a team.
- * @param {Number} teamId - The team ID.
- * @param {String} guildId - The guild ID.
- */
-const getTeamData = async (teamId, guildId) => {
-
-  const url = `${process.env.SEMPER_API_URL}team/get/data/?teamId=${teamId}&guildId=${guildId}&accessToken=${process.env.SEMPER_API_ACCESS_TOKEN}`;
-
-  try {
-
-    const response = await fetch(url, { method: 'GET' });
-    const data = await response.json();
-
-    return data;
-
-  } catch (error) {
-
-    console.log(error);
-    return error;
-
-  }
-
-};
-
-module.exports = getTeamData;
+module.exports = { getTeamData, getTeamList };
