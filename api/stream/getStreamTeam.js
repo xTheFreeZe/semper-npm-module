95500e5f920537eb892dfcceaf4a3d3414ed3d73
Marwin <marwin.eder@gmx.de>
2023-06-23 15:51:06 +0200

    getStreamTeam - teamSlot

--- api/stream/getStreamTeam.js
+++ api/stream/getStreamTeam.js
@@ -1,10 +1,18 @@
 /**
+ * @param {number} teamSlot The team slot to get the data for.
  * @description Gets the team data from the Semper API - FOR STREAMS
  * @returns {Promise} Returns a promise that resolves to the team data object or false if the request fails.
  */
-const getStreamTeam = async () => {
+const getStreamTeam = async (teamSlot) => {
 
-  const url = `${process.env.SEMPER_API_URL}/stream/team/data?accessToken=${process.env.SEMPER_API_ACCESS_TOKEN}`;
+  if ( teamSlot === undefined) {
+
+    console.error(`[ERROR] No team slot specified - Expected a number but got ${teamSlot} - getStreamTeam()`);
+    return false;
+
+  }
+
+  const url = `${process.env.SEMPER_API_URL}/stream/team/data?teamSlot=${teamSlot}&accessToken=${process.env.SEMPER_API_ACCESS_TOKEN}`;
 
   try {
 
--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const editSkillRating = require('./api/teams/editSkillRating.js');
 const editLFPList = require('./api/teams/editLFPList.js');
 const setPlayerData = require('./api/teams/setPlayerData.js');
 
-module.exports = { getTeamData, getTeamList, getFeedbackList, editSkillRating, editLFPList, setPlayerData };
+const getCasterData = require('./api/stream/getCasterData.js');
+const getEventData = require('./api/stream/getEventData.js');
+const getMappoolData = require('./api/stream/getMappoolData.js');
+const getSocialData = require('./api/stream/getSocialData.js');
+const getStreamTeam = require('./api/stream/getStreamTeam.js');
+const getTeamLineUp = require('./api/stream/getTeamLineUp.js');
+
+module.exports = { getTeamData, getTeamList, getFeedbackList, editSkillRating, editLFPList, setPlayerData, getCasterData, getEventData, getMappoolData, getSocialData, getStreamTeam, getTeamLineUp };
